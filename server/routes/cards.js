be0d84442a0e7b233a7f2f4bedac150425fada96
Arthur <kingart808@gmail.com>
2020-01-18 17:50:30 -1000

    done with crud routes for cards

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
     })
     .catch(err => {
       console.log(err);
-      res.json({status:500,message:'Could not find any cards.'})
+      res.json({ status: 500, message: "Could not find any cards." });
     });
 });
 
@@ -34,4 +34,41 @@ router.post("/", (req, res) => {
     });
 });
 
+router.put("/:id", (req, res) => {
+  let newCard = {
+    title: req.body.title,
+    body: req.body.body,
+    priority_id: req.body.priority,
+    status_id: req.body.status,
+    created_by: 1,
+    assigned_to: 2
+  };
+  return req.db.Cards.forge({ id: req.params.id })
+    .save(newCard)
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      console.log(err);
+      res.json({
+        status: 500,
+        message: "Unable to edit card, please try again later."
+      });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  return req.db.Cards.where({ id: req.params.id })
+    .destroy()
+    .then(() => {
+        res.json({status:200, message:'You have Deleted the card!'})
+    })
+    .catch(err => {
+      console.log(err);
+      res.json({
+        status: 500,
+        message: "Unable to Delete card, please try again later."
+      });
+    });
+});
 module.exports = router;
